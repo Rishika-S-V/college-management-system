a1089290812c6d30a179bae1160a80dceb968793
Naveen G <naveeng2402@gmail.com>
2022-01-17 13:50:29 +0530

    Fix: main.py

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 from flask import Flask, render_template
+from website import app
 from livereload import Server
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return render_template("index.html")
 
  
 if __name__ == "__main__":
